603102fa07967b4e6a728a0b01a5e94161627a27
Tony Guerra <guerra.anthony122@gmail.com>
2025-07-07 20:45:37 -0300

    [TEST]

--- tests/Unit/ProductServiceTest.php
+++ tests/Unit/ProductServiceTest.php
@@ -4,22 +4,37 @@ namespace Tests\Unit;
 
 use App\DTOs\ProductDTO;
 use App\Services\ProductService;
-use Override;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class ProductServiceTest extends TestCase
 {
     private ProductService $service;
 
-    #[Override]
     protected function setUp(): void
     {
         parent::setUp();
+
         $this->service = app(ProductService::class);
     }
 
     public function testFetchProducts(): void
     {
+        Cache::shouldReceive('remember')
+            ->once()
+            ->andReturn([
+                [
+                    'id' => 1,
+                    'title' => 'Produto A',
+                    'price' => 10.0,
+                    'description' => 'Descrição A',
+                    'category' => 'Categoria A',
+                    'image' => 'image-a.jpg',
+                    'rating' => ['rate' => 4.5, 'count' => 10],
+                ]
+            ]);
+
         $products = $this->service->fetchProducts();
 
         $this->assertIsArray($products);
@@ -29,7 +44,32 @@ class ProductServiceTest extends TestCase
 
     public function testFetchManyProductsByIds(): void
     {
-        $ids = [1, 2, 3];
+        $data = [
+            [
+                'id' => 1,
+                'title' => 'Produto A',
+                'price' => 10.0,
+                'description' => 'Descrição A',
+                'category' => 'Categoria A',
+                'image' => 'image-a.jpg',
+                'rating' => ['rate' => 4.5, 'count' => 10],
+            ],
+            [
+                'id' => 2,
+                'title' => 'Produto B',
+                'price' => 20.0,
+                'description' => 'Descrição B',
+                'category' => 'Categoria B',
+                'image' => 'image-b.jpg',
+                'rating' => ['rate' => 4.2, 'count' => 8],
+            ],
+        ];
+
+        Cache::shouldReceive('remember')
+            ->once()
+            ->andReturn($data);
+
+        $ids = [1, 2];
         $products = $this->service->fetchManyProductsByIds($ids);
 
         $this->assertIsArray($products);
@@ -42,6 +82,20 @@ class ProductServiceTest extends TestCase
 
     public function testFetchProductById(): void
     {
+        Cache::shouldReceive('remember')
+            ->once()
+            ->andReturn([
+                [
+                    'id' => 1,
+                    'title' => 'Produto A',
+                    'price' => 10.0,
+                    'description' => 'Descrição A',
+                    'category' => 'Categoria A',
+                    'image' => 'image-a.jpg',
+                    'rating' => ['rate' => 4.5, 'count' => 10],
+                ]
+            ]);
+
         $id = 1;
         $product = $this->service->fetchProductById($id);
 
